d2bdbf364a542fa39da5e8e1902794c9606006b5
meep@UNIT-01 <meepdottech@gmail.com>
2022-10-23 22:22:56 -0400

    initial custom file extensions plugin code

--- main.ts
+++ main.ts
@@ -1,116 +1,61 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CustomFileExtensionsSettings {
+  additionalFileTypes: string;
+  previousAdditionalFileTypes: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CustomFileExtensionsSettings = {
+  additionalFileTypes: '{markdown: ["", "txt", "html", "js", "css", "ts", "yaml"]}',
+  previousAdditionalFileTypes: '{markdown: ["", "txt", "html", "js", "css", "ts", "yaml"]}'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CustomFileExtensions extends Plugin {
+	settings: CustomFileExtensionsSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    super.onload();
+    await this.loadSettings();
+    this.addSettingTab(new CustomFileExtensionsSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.apply();
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
+  onunload() {
+    this.revert();
+  }
+  
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
+  }
+  
 	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    await this.saveData(this.settings);
+    this.apply();
+  }
+  
+  apply() {
+    this.revert();
+
+    // apply new types:
+    const views = JSON.parse(this.settings.additionalFileTypes);
+    for (const view in views) {
+      this.registerExtensions([views[view]], view);
+    }
+  }
+
+  revert() {
+    for (const view in Object.values(this.plugin.settings.additionalFileTypes).flat()) {
+      this.registerExtensions(view, "");
+    }
+    this.registerExtensions([".md"], 'markdown');
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CustomFileExtensionsSettingTab extends PluginSettingTab {
+	plugin: CustomFileExtensions;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CustomFileExtensions) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +65,17 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Custom File Extensions Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Config')
+			.setDesc("Valid entry is a JSON object with properties named after the desired view, containing the file types to assign to that view. EX: " + DEFAULT_SETTINGS.additionalFileTypes)
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+				.setPlaceholder(DEFAULT_SETTINGS.additionalFileTypes)
+				.setValue(this.plugin.settings.additionalFileTypes)
+        .onChange(async (value) => {
+          this.plugin.settings.previousAdditionalFileTypes = JSON.parse(this.plugin.settings.additionalFileTypes);
+					this.plugin.settings.additionalFileTypes = value;
 					await this.plugin.saveSettings();
 				}));
 	}
